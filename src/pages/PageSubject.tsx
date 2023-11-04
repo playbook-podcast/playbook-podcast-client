371f70c7a08776aab08b7f461be9d3734f3367e4
yehor <foreman270478@gmail.com>
2023-11-04 21:28:08 +0200

    feat: add subject page content

--- src/pages/PageSubject.tsx
+++ src/pages/PageSubject.tsx
@@ -1,8 +1,38 @@
-import { Box } from '@mui/material';
+import { Box, Paper, Switch, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+import { getSubject } from '../api';
+import { ISubject } from '../types';
 
 export const PageSubject = () => {
   const { subjectId } = useParams();
 
-  return <Box>Subject page with id {subjectId}</Box>;
+  const [subject, setSubject] = useState<ISubject | null>(null);
+
+  const handleSwitchChange = () => {
+    // switch mode
+  };
+
+  useEffect(() => {
+    if (subjectId) {
+      const fetchedSubject = getSubject(subjectId);
+
+      setSubject(fetchedSubject);
+    }
+  }, []);
+
+  return (
+    <Box>
+      <Paper elevation={3} sx={{ p: 4 }}>
+        <Box width="100%" display="flex" justifyContent="flex-end">
+          <Switch onChange={handleSwitchChange} />
+        </Box>
+        <Typography variant="h4" component="h2" gutterBottom>
+          {subject?.title}
+        </Typography>
+        <Typography>{subject?.body}</Typography>
+      </Paper>
+    </Box>
+  );
 };
