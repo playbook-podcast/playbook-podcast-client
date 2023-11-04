c711b382522a5e1d61c939abe4fa3ed19f5cfd47
Mykhailo Andrushchenko <andr.mv97@gmail.com>
2023-11-05 01:52:28 +0200

    fix: add missed props to subject interface

--- src/api/subjects/createSubject.ts
+++ src/api/subjects/createSubject.ts
@@ -10,6 +10,7 @@ export const createSubject = ({ title, description }: CreateSubject): ISubject =
     id: 1,
     title: 'The Future of Renewable Energy',
     body: 'As the world increasingly seeks sustainable sources of energy, the future of renewable energy looks bright. With advances in solar technology, wind energy efficiencies, and the untapped potential of tidal and geothermal power, the landscape of energy production is rapidly changing. This article explores the innovations driving this transformation and what we might expect in the coming decades.',
+    bodyParsed: [],
     summary:
       'Exploration of the advances and future potential of renewable energy sources such as solar, wind, tidal, and geothermal power.',
     audioStatus: EAudioStatus.DONE,
